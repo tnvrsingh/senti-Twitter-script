22a2c28cdb911d5cf023be6780bc04086bf08089
Tanvir Singh <tanvirs137@gmail.com>
2017-02-10 02:46:35 +0530

    Update senti-Twitter.R

--- senti-Twitter.R
+++ senti-Twitter.R
@@ -1,50 +1,72 @@
 library(twitteR)
-library(RCurl)
-library(RJSONIO)
 library(syuzhet)
-library(lubridate)
+library(plyr)
 library(ggplot2)
-library(scales)
-library(reshape2)
-library(dplyr)
-library(stringr)
-library(tm)
-library(purrr)
+library(wordcloud)
+library(RColorBrewer)
 
+# Declare Twitter API Credentials
 api_key <- "" # Get this credential from dev.twitter.com and enter it here
 api_secret <- "" # Get this credential from dev.twitter.com and enter it here
 token <- "" # Get this credential from dev.twitter.com and enter it here
 token_secret <- "" # Get this credential from dev.twitter.com and enter it here
 
-tweets <- searchTwitter("trump wall", n=100)
+# Create Twitter Connection
+setup_twitter_oauth(api_key, api_secret, token, token_secret)
 
-print (tweets)
+# search some tweets
+some_tweets = searchTwitter("demonetisation", n=1500, lang="en")
 
-tweets.df <- twListToDF(tweets)
+# get the tweets text
+tweetsText = sapply(some_tweets, function(x) x$getText())
 
-tweets = tolower(tweets)
+# remove retweet entities - retweet and via so that they don't interfere
+tweetsText = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweetsText)
+# remove at people
+tweetsText = gsub("@\\w+", "", tweetsText)
+# remove punctuation
+tweetsText = gsub("[[:punct:]]", "", tweetsText)
+# remove numbers
+tweetsText = gsub("[[:digit:]]", "", tweetsText)
+# remove html links
+tweetsText = gsub("http\\w+", "", tweetsText)
+# remove unnecessary spaces
+tweetsText = gsub("[ \t]{2,}", "", tweetsText)
+tweetsText = gsub("^\\s+|\\s+$", "", tweetsText)
 
-tweets_cl <- gsub('\\p{So}|\\p{Cn}', '', tweets, perl = TRUE)
+# define "tolower error handling" function
+try.error = function(x)
+{
+   # create missing value
+   y = NA
+   # tryCatch error
+   try_error = tryCatch(tolower(x), error=function(e) e)
+   # if not an error
+   if (!inherits(try_error, "error"))
+   y = tolower(x)
+   # result
+   return(y)
+}
+# lower case using try.error with sapply
+tweetsText = sapply(tweetsText, try.error)
 
-#map_chr converts to Vector
-tweetaftermap <- map_chr(tweets, function(x) x $text)
+# remove NAs in tweetsText
+tweetsText = tweetsText[!is.na(tweetsText)]
+names(tweetsText) = NULL
 
-mySentiment <- get_nrc_sentiment(tweetaftermap)
+Sentiment <- get_nrc_sentiment(tweetsText)
+head(Sentiment)
 
-head(mySentiment)
+print("Sentiment analysis done!")
 
-print ("sentiment analysis done!")
-
-# ---------- OPTIONAL CODE for generating a PDF of the sentiment analysis
-
-# tweets <- cbind(tweets_cl, mySentiment)
-# print ("binding to sentiment done")
-# sentimentTotals <- data.frame(colSums(tweets_cl[,c(11:18)]))
-# print("data frame column shit done")
-# names(sentimentTotals) <- "count"
-# sentimentTotals <- cbind("sentiment" = rownames(sentimentTotals), sentimentTotals)
-# rownames(sentimentTotals) <- NULL
-# ggplot(data = sentimentTotals, aes(x = sentiment, y = count)) +
-#         geom_bar(aes(fill = sentiment), stat = "identity") +
-#         theme(legend.position = "none") +
-#         xlab("Sentiment") + ylab("Total Count") + ggtitle("Total Sentiment Score for All Tweets")
+tweets <- cbind(tweetsText, Sentiment)
+print ("binding to sentiment done")
+sentimentTotals <- data.frame(colSums(tweetsText[,c(11:18)]))
+print("data frame column shit done")
+names(sentimentTotals) <- "count"
+sentimentTotals <- cbind("sentiment" = rownames(sentimentTotals), sentimentTotals)
+rownames(sentimentTotals) <- NULL
+ggplot(data = sentimentTotals, aes(x = sentiment, y = count)) +
+        geom_bar(aes(fill = sentiment), stat = "identity") +
+        theme(legend.position = "none") +
+        xlab("Sentiment") + ylab("Total Count") + ggtitle("Total Sentiment Score for All Tweets")
